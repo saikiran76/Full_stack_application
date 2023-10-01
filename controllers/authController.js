ab88376d4c07fec956f095aeaa86b79b2345aad9
unknown <kskkorada76@gmail.com>
2023-10-01 19:30:22 +0530

    AuthController - login

--- controllers/authController.js
+++ controllers/authController.js
@@ -52,3 +52,70 @@ export const signUp = asyncHandler(async (req, res) => {
     })
 
 })
+
+
+/*
+
+@LOGIN
+@route http://localhost:/4000/api/auth/login
+@description: User Login Controller for logging user
+@parameters name, email, password
+@returns User Object
+
+*/
+
+export const login = asyncHandler(async (req, res)=>{
+    const {name, email, password} = req.body
+
+    if(!email || !password){
+        throw new CustomError('Please fill all the fields', 400);
+    }
+
+    const user = User.findOne({email}).select("+password")
+
+    if(!user){
+        throw new CustomError("Invalid Credentials", 400)
+    }
+
+    const isPasswordMatched = await user.comparePassword(password)
+
+    if(isPasswordMatched){
+        const token = user.getJwtToken()
+        user.password = undefined;
+        res.cookie("token", token, cookieOptions)
+        return res.staus(200).json({
+            success: true,
+            token,
+            user
+        })
+
+
+    }
+
+    throw new CustomError('Invalid credentials - pass', 400)
+
+} )
+
+/*
+
+@SIGNOUT
+@route http://localhost:/4000/api/auth/logout
+@description: User SignOut
+@parameters 
+@returns success message
+
+*/
+
+export const logout = asyncHandler(async(_req, res)=>{
+    // res.clearCookie( )
+    res.cookie("token", null, {
+        expires: new Date(Date.now()),
+        httpOnly: true
+    })
+
+    res.status(200).json({
+        success: true,
+        message: "Logged Out"
+    })
+})
+

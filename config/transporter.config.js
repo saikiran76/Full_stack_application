495aa76b1588b69c86305f49060355726d21100d
unknown <kskkorada76@gmail.com>
2023-10-02 14:29:18 +0530

    Added product controller whereas order and product controller are still in process

--- config/transporter.config.js
+++ config/transporter.config.js
@@ -4,9 +4,8 @@ import config from './index'
 const transporter = nodemailer.createTransport({
     host: config.SMTP_MAIL_HOST,
     port: config.SMTP_MAIL_PORT,
-    secure: false,
+    secure: false, // true for 465, false for other ports
     auth: {
-      // TODO: replace `user` and `pass` values from <https://forwardemail.net>
       user: config.SMTP_MAIL_USERNAME,
       pass: config.SMTP_MAIL_PASSWORD,
     },
